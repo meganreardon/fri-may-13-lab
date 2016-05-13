9b0d62dec3907d284342b7a81423ccd45e400062
Megan Reardon <meganr@gmail.com>
2016-05-13 14:15:26 -0700

    completed problem2

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function sum(a,b){
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 
@@ -27,10 +27,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(){}
+function multiply(a,b){
+  var productResult = a * b;
+  var productMessage = 'The product of ' + a + ' and ' + b + ' is ' + productResult + '.';
+  return [productResult, productMessage];
+}
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(4,7);
+testMultiply(4,7);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 
